772b8558638c0109f8cc9be90927362e1be9ac4d
Camilo2B <camilo.ruizl@uqvirtual.edu.co>
2025-03-02 22:30:28 -0500

    Delete Hotel_Trabajo_Seguimiento directory

--- Hotel_Trabajo_Seguimiento/src/main/java/co/edu/uniquindio/poo/hotel_trabajo_seguimiento/Consumir.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uniquindio.poo.hotel_trabajo_seguimiento;
-
-public interface Consumir {
-
-    void consumir();
-
-}
--- Hotel_Trabajo_Seguimiento/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.poo.hotel_trabajo_seguimiento {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.edu.uniquindio.poo.hotel_trabajo_seguimiento to javafx.fxml;
-    exports co.edu.uniquindio.poo.hotel_trabajo_seguimiento;
-}
